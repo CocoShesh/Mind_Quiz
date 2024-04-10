08c2c5045848f4a9e509e462f32b9315667fd840
NARD <glenardrefamonte1@gmail.com>
2024-04-10 20:05:48 +0800

    Aded logo and anchor tag for the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <nav className="flex items-center justify-center pt-5">
-      <span className="custom-text text-5xl font-bold">Mind Quiz</span>
+      <img src="/mind-quiz-logo.png" alt="" className="h-20" />
+      <span className="custom-text text-5xl font-bold">
+        <a href="/">Mind Quiz </a>
+      </span>
     </nav>
   );
 };
