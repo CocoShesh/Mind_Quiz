1be624da72c93233c904b9273f2eb4259212b6b9
NARD <glenardrefamonte1@gmail.com>
2024-04-16 14:21:14 +0800

    Feat: Added conditional rendering to handle errors when the category is null

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -3,7 +3,7 @@ import { getQuizQuestions } from "../../Api/Quiz";
 import { useOptions } from "../../Context/OptionsContext";
 import { useNavigate } from "react-router-dom";
 import Modal from "../../components/modal/Modal";
-
+import ErrorMessage from "../../components/Error/ErrorMessage";
 const Quiz = () => {
   const navigate = useNavigate();
   const {
@@ -81,46 +81,51 @@ const Quiz = () => {
   return (
     <>
       <section className="flex flex-col w-full  justify-center h-full items-center  max-sm:px-3 pb-10">
-        <section className="w-[500px] h-fit max-sm:w-full   mt-10 bg-white shadow-lg p-5 rounded-xl">
-          {isLoading && <div className="custom-loader flex mx-auto"></div>}
-          {questions?.length > 0 && (
-            <>
-              <h2>
-                {questionIndex + 1} of {questions?.length} questions
-              </h2>{" "}
-              <br />
-              <hr />
-              <p className="my-5">{questions[questionIndex]?.question}</p>
-              {Object.values(questions[questionIndex]?.answers)
-                .filter(answer => answer !== null)
-                .map((answer, index) => (
-                  <div
-                    key={index}
-                    onClick={() => setSelectedAnswers({ answer, index })}
-                    className={`w-full h-fit cursor-pointer text-[#806788] border border-[#cbcbcb] mb-5 px-3 py-2 rounded-md hover:bg-[#ffdde2] ${
-                      selectedAnswers && selectedAnswers.index === index
-                        ? "bg-[#ffdde0]  "
-                        : ""
-                    }`}
-                  >
-                    {answer}
-                  </div>
-                ))}
-              <button
-                onClick={handleNextQuestion}
-                disabled={selectedAnswers === null}
-                className={`flex ml-auto  p-4 text-white rounded-md  ${
-                  selectedAnswers === null
-                    ? " bg-red-500  cursor-not-allowed "
-                    : "bg-[#0c68ed]  "
-                }`}
-              >
-                Next Question
-              </button>
-            </>
-          )}
-        </section>
+        {isLoading && <div className="custom-loader flex mx-auto"></div>}
+        {selectedCategory === null ? (
+          <ErrorMessage />
+        ) : (
+          <section className="w-[500px] h-fit max-sm:w-full   mt-10 bg-white shadow-lg p-5 rounded-xl">
+            {questions?.length > 0 && (
+              <>
+                <h2>
+                  {questionIndex + 1} of {questions?.length} questions
+                </h2>{" "}
+                <br />
+                <hr />
+                <p className="my-5">{questions[questionIndex]?.question}</p>
+                {Object.values(questions[questionIndex]?.answers)
+                  .filter(answer => answer !== null)
+                  .map((answer, index) => (
+                    <div
+                      key={index}
+                      onClick={() => setSelectedAnswers({ answer, index })}
+                      className={`w-full h-fit cursor-pointer text-[#806788] border border-[#cbcbcb] mb-5 px-3 py-2 rounded-md hover:bg-[#ffdde2] ${
+                        selectedAnswers && selectedAnswers.index === index
+                          ? "bg-[#ffdde0]  "
+                          : ""
+                      }`}
+                    >
+                      {answer}
+                    </div>
+                  ))}
+                <button
+                  onClick={handleNextQuestion}
+                  disabled={selectedAnswers === null}
+                  className={`flex ml-auto  p-4 text-white rounded-md  ${
+                    selectedAnswers === null
+                      ? " bg-red-500  cursor-not-allowed "
+                      : "bg-[#0c68ed]  "
+                  }`}
+                >
+                  Next Question
+                </button>
+              </>
+            )}
+          </section>
+        )}
       </section>
+
       {openModal && (
         <Modal handleClose={handleCloseModal} isCorrect={isCorrect} />
       )}
